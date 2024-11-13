b5123860270d4a3b5f4743288f1ecde1daf64812
Kitty <parkhabit@gmail.com>
2024-11-13 17:40:48 +1100

    add basic types

---
+++ src/types/index.ts
@@ -0,0 +1,34 @@
+import FeatherIcon from "@expo/vector-icons/Feather";
+
+export enum ValueType {
+    BOOLEAN,
+    NUMBER,
+}
+
+export enum Frequency {
+    DAILY = 'daily',
+    WEEKLY = 'weekly',
+    MONTHLY = 'monthly',
+}
+
+export type Habit = {
+    id: number;
+    name: string;
+    description: string;
+    icon: keyof typeof FeatherIcon.glyphMap;
+    valueType: ValueType;
+    frequency: Frequency;
+    reminders: string[];
+}
+
+export type TrackedHabit = {
+    habitId: number;
+    habitValue: boolean | number;
+    habitComplete: boolean;
+}
+
+export type TrackedDay = {
+    id: number;
+    date: string;
+    habits: TrackedHabit[]
+}
