444712ba744cdc8962d546a45d678d030b87b536
Kitty <parkhabit@gmail.com>
2024-11-13 13:34:41 +1100

    add different greeting according to time of day

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,10 +1,18 @@
 import { Text, View } from "react-native";
 
 const Greeting = ({ name }: { name: string }) => {
-  // TODO: add greeting according to the time of day
+  const timeofDay = new Date().getHours();
+  let greeting = "Good morning";
+  if (timeofDay >= 12 && timeofDay < 18) {
+    greeting = "Good afternoon";
+  } else if (timeofDay >= 18 && timeofDay < 24) {
+    greeting = "Good evening";
+  }
+
   return (
-    <View>
-      <Text>{name}</Text>
+    <View className="pl-3 pt-16 pb-6">
+      <Text className="font-semibold text-3xl">{greeting},</Text>
+      <Text className="font-semibold text-3xl">{name}</Text>
     </View>
   );
 };
