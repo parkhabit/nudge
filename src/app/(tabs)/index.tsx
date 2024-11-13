897d34b1980bbb400a4904dae0ebf3fe4b521141
Kitty <parkhabit@gmail.com>
2024-11-13 17:42:59 +1100

    render hardcoded list of habits

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,7 +1,41 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { FlatList, Text, SafeAreaView } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Greeting from "../../components/Greeting";
+import HabitCard from "../../components/HabitCard";
+import { dataTracking, dataHabits } from "../../data";
+import { Habit, TrackedDay, TrackedHabit } from "../../types";
 
 const Home = () => {
-  return <Text>Home</Text>;
+  const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
+  const user = { name: "Kitty" };
+
+  const currentDateHabitIds = dataTracking.filter(
+    (tracking: TrackedDay) => tracking.date === date
+  )[0].habits;
+
+  let renderHabits: Habit[] = [];
+  dataHabits.filter((habit) => {
+    currentDateHabitIds.map((trackableHabit) => {
+      if (habit.id === trackableHabit.habitId) {
+        renderHabits.push(habit);
+      }
+    });
+  });
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView>
+        <Greeting name={user.name} />
+        {/* <DatePicker /> */}
+        <FlatList
+          contentContainerClassName="gap-4 p-2"
+          data={renderHabits}
+          renderItem={({ item }) => <HabitCard habit={item} />}
+        />
+      </SafeAreaView>
+    </GestureHandlerRootView>
+  );
 };
 
 export default Home;
---
+++ src/components/HabitCard.tsx
@@ -0,0 +1,86 @@
+import { Text, View, TouchableOpacity, Animated } from "react-native";
+import { Habit, ValueType } from "../types";
+import FeatherIcon from "@expo/vector-icons/Feather";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+
+const HabitCard = ({
+  habit: { id, name, description, icon, valueType, frequency, reminders },
+}: {
+  habit: Habit;
+}) => {
+  const onPressSkip = () => {
+    // skip habit for this day
+    // update the tracking table, need trackingId habitId and set the habitComplete to false
+  };
+
+  const onPressTracked = () => {
+    if (valueType === ValueType.NUMBER) {
+      // show a modal to input the number or time
+    } else {
+      // update the tracking table, need trackingId habitId and set the habitValue and habitComplete to true
+    }
+  };
+
+  const renderRightActions = (
+    dragX: Animated.AnimatedInterpolation<string | number>,
+    onPressSkip: () => void
+  ) => {
+    const scale = dragX.interpolate({
+      inputRange: [-100, -50, 0],
+      outputRange: [2, 1, 0],
+      extrapolate: "clamp",
+    });
+    return (
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={() => onPressSkip()}
+        className="flex flex-[0.3] items-center justify-center bg-blue-300"
+      >
+        <Text>Skip</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const renderLeftActions = (
+    dragX: Animated.AnimatedInterpolation<string | number>,
+    onPressTracked: () => void
+  ) => {
+    const trans = dragX.interpolate({
+      inputRange: [0, 50, 100, 101],
+      outputRange: [-20, 0, 0, 1],
+    });
+    return (
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={() => onPressTracked()}
+        className="flex flex-[0.3] items-center justify-center bg-green-300"
+      >
+        <Text>Track habit</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <Swipeable
+      overshootRight={true}
+      overshootLeft={true}
+      renderRightActions={(dragX) => renderRightActions(dragX, onPressSkip)}
+      renderLeftActions={(dragX) => renderLeftActions(dragX, onPressTracked)}
+    >
+      <View
+        key={id}
+        className="bg-gray-200 rounded-lg p-3 flex flex-row items-center gap-2"
+      >
+        <View className="rounded-full bg-black p-2">
+          <FeatherIcon size={28} name={icon} color={"white"} />
+        </View>
+        <View>
+          <Text className="font-semibold">{name}</Text>
+          <Text>{description}</Text>
+        </View>
+      </View>
+    </Swipeable>
+  );
+};
+
+export default HabitCard;
